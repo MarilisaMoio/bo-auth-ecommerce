be696c944b2cdc61781a0e89da667ac01819a37b
Marilisa Moio <moiomarilisa@gmail.com>
2024-06-03 17:32:15 +0200

    feat: add Products migration and seeder

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+use App\Models\Product;
 
 class ProductSeeder extends Seeder
 {
@@ -12,8 +14,20 @@ class ProductSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        for($i = 0; $i < 10; $i++){
+            $newProduct = new Product();
+            $newProduct->name = $faker->words(3, true);
+            $newProduct->price = $faker->randomFloat(2, 5, 80);
+            $newProduct->description = $faker->paragraphs(2, true);
+            $newProduct->img = $faker->imageUrl(640, 480, 'animals', true);
+            $newProduct->category = $faker->words(3, true);
+            $newProduct->is_discounted = $faker->boolean();
+            if($newProduct->is_discounted){
+                $newProduct->discount = $faker->numberBetween(15, 50);
+            };
+            $newProduct->save();
+        }
     }
 }
