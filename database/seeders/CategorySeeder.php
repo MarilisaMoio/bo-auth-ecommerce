daa66abc248856c29ec9edc0229469255a679259
DanieleBegnis <daniele.begnis@gmail.com>
2024-06-07 16:17:43 +0200

    feat add category msm

---
+++ database/seeders/CategorySeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+use App\Models\Category;
+
+class CategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $categories = ['Sport e Tempo libero', 'Cucina', 'Hobby', 'Animali', 'Modellismo', 'Vestiti e Accessori'];
+        foreach ($categories as $category) {
+            $newCategory = new Category();
+            $newCategory->name = $category;
+            $newCategory->slug = Str::slug($category, '-');
+            $newCategory->save();
+        }
+    }
+}
